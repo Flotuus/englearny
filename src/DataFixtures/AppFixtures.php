6f5b056655d6db868b2aa79c0194acbc7d943035
bayon <fabienbayon@yahoo.fr>
2020-12-10 15:14:22 +0000

    erreur ou pas

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,7 +14,6 @@ use App\Entity\Test;
 class AppFixtures extends Fixture
 {
     private $manager;
-    private $repoTheme;
     private $faker;
 
     public function __construct(){
@@ -31,20 +30,16 @@ class AppFixtures extends Fixture
     }
 
     public function loadThemes(){
+        $listeTheme = ["Animaux","Corps Humain","Vegetation","Legumes","Fruits","Automobile","Technologies","Meubles","Batiments" ];
+
         for($i=0;$i<10;$i++){
             $theme = new Theme();
-            $theme->setLibelle($this->)
+            $theme->setLibelle($listeTheme[$i])
      
-            $this->addReference('user'.$i, $user);
-            $this->manager->persist($user);
+            $this->addReference('theme'.$i, $theme);
+            $this->manager->persist($theme);
         }
-        $user = new User();
-        $user->setNom('LE GALES')
-        ->setPrenom('julien')
-        ->setEmail('julien.legales@gmail.com')
-        ->setPassword('julien')
-        ->setDateInscription(new \DateTime());
-        $this->addReference('julien', $user);
+    
         $this->manager->flush();
 
     }
