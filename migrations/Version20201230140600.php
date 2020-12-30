9330498b5a9df764a004652df79df6cd629354f1
bayon <fabienbayon@yahoo.fr>
2020-12-30 14:20:57 +0000

    migration

--- migrations/Version20201230140600.php
+++ migrations/Version20201230140600.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201222101432 extends AbstractMigration
+final class Version20201230140600 extends AbstractMigration
 {
     public function getDescription() : string
     {
